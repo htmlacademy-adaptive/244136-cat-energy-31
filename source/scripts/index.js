1ba08d4cdf187f059292e083e153109815c79aeb
Kelluin <156843280+Kelluin@users.noreply.github.com>
2024-05-09 20:37:28 +0300

    Векторная графика (#6)
    * Добавляет векторную графику в index
    
    * добавление всей векторной графики

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,5 @@
 /* в этот файл добавляет скрипты*/
+document.querySelector('.nav-button').addEventListener('click', () => {
+  document.querySelector('.site-nav__list').classList.toggle('site-nav__list--opened');
+  document.querySelector('.nav-button').classList.toggle('nav-button--opened');
+});
